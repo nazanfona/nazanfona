083c06ea4459e90bbe97ad678b0e63c456a0b10c
Esperanza González <esperanza@mechaphysis.org>
2025-07-01 22:25:10 +0200

    feat: add entry codes from googleForms

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,24 +1,31 @@
+const formURL = "https://docs.google.com/forms/d/e/1FAIpQLSdKOxV2cs-TiIz1Lmjj9c421Uh-FQ05jPRTC48pRly2JYZXnA/formResponse"
 const googleFormEntries = new Map([
-  ["name", ""],
-  ["lastName", ""],
-  ["idNumber", ""],
-  ["birthDate", ""],
-  ["email", ""],
-  ["phoneNumber", ""],
-  ["streetAddress", ""],
-  ["postalCode", ""],
-  ["city", ""],
-  ["region", ""],
-  ["fee", ""],
-  ["bankNumber", ""],
-  ["fullNameBankAccountOwner", ""],
-  ["idNumberBankAccountOwner", ""],
+  ["name", "2013521895"],
+  ["lastName", "1758078663"],
+  ["idNumber", "2005431508"],
+  ["birthDate", "555002916"],
+  ["email", "811220221"],
+  ["phoneNumber", "356561953"],
+  ["streetAddress", "1171888018"],
+  ["postalCode", "586063522"],
+  ["city", "825585537"],
+  ["region", "802463913"],
+  ["fee", "357527510"],
+  ["bankNumber", "995604611"],
+  ["fullNameAccountOwner", "214980461"],
+  ["idNumberAccountOwner", "572879379"],
 ]);
 
-function setInputName(value, key, map) {
+
+
+addEventListener("DOMContentLoaded", () => {
+
+  function setInputName(value, key, map) {
   let input = document.getElementById(key);
-  input.setAttribute("name", value);
+  input.setAttribute("name", "entry."+ value);
 }
 
-addEventListener("DOMContentLoaded", formEntries.forEach(setInputName));
+  document.getElementById("altaSocio").setAttribute("action", formURL)
+  googleFormEntries.forEach(setInputName)
+});
 
